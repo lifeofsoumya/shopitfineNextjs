f6deb0b0f59a8aed68b61aff8308ef6ddd591be3
lifeofsoumya <mondalsoumya2001@gmail.com>
2023-06-01 19:08:11 +0530

    simple images added

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,6 +2,8 @@ import Link from 'next/link'
 import MyHead from '../components/MyHead'
 import CollectEmail from '../components/CollectEmail'
 
+const gamepadImg = ['GamePad.jpg', 'GamePad2.jpg', 'GamePad3.jpg']
+
 export default function Home() {
   return (
     <>
@@ -18,9 +20,18 @@ export default function Home() {
           <p className='tag-line md:text-xl'>Compare prices of all sites for the same product, <span>Save Money</span></p>
           <CollectEmail/>
           <img className='w-full md:w-3/6 mt-8' src="/images/sitehero.png" alt="" />
-
-
         </div>
+      </section>
+      <section>
+          <h2 className='text-2xl md:text-3xl text-center font-bold text-white pb-3'>Explore <span className='headline'>Trending</span> Products</h2>
+          <hr className="w-40 mx-auto h-3 my-4" />
+          <div className="prod w-8/12 mx-auto flex flex-col md:flex-row gap-3">
+            {gamepadImg.map((img, i) => (
+              <span className=''>
+                <img key={i} className='w-full md:w-8/12 mx-auto rounded-xl hover:scale-105 transition-all duration-300 cursor-pointer' src={`/images/products/game/${img}`} alt="" />
+              </span>
+            ))}
+          </div>
       </section>
     </>
   )
